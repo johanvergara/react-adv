96d905664eb230cec47bcb122688bb1bc42e65a5
Johan <vjohan647@gmail.com>
2022-04-12 10:43:17 -0500

    Creacion de nueva rama para lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes, NavLink, Navigate } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 import logo from '../logo.svg';
 
@@ -11,30 +12,30 @@ export const Navigation = () => {
                     <ul>
                         <li>
                             <NavLink 
-                                to="/home" 
+                                to="/lazy1" 
                                 className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-                            >Home</NavLink>
+                            >Lazy 1</NavLink>
                         </li>                    
                         <li>
                             <NavLink 
-                                to="/about"
+                                to="/lazy2"
                                 className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-                            >About</NavLink>
+                            >Lazy 2</NavLink>
                         </li>                    
                         <li>
                             <NavLink 
-                                to="/users"
+                                to="/lazy3"
                                 className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-                            >Users</NavLink>
+                            >Lazy 3</NavLink>
                         </li>                    
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="home" element={ <h1> Home Page </h1> } />
-                    <Route path="about" element={ <h1>About Page</h1> } />
-                    <Route path="users" element={ <h1>Users Page</h1> } />
+                    <Route path="lazy1" element={ <LazyPage1 /> } />
+                    <Route path="lazy2" element={ <LazyPage2 /> } />
+                    <Route path="lazy3" element={ <LazyPage3 /> } />
 
-                    <Route path="/*" element={ <Navigate to="/home" replace /> } />
+                    <Route path="/*" element={ <Navigate to="/lazy1" replace /> } />
                 </Routes>
             </div>
         </BrowserRouter>
